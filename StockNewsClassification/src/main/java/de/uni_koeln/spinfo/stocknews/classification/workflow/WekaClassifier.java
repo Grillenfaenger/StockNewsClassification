e3bf8e3e4ed86abc1d230840d9a60fd8c6519528
avogt <mail.andreasvogt@gmail.com>
2017-06-22 15:19:37 +0200

    choose classifier by name simplyfied

--- StockNewsClassification/src/main/java/de/uni_koeln/spinfo/stocknews/classification/workflow/WekaClassifier.java
+++ StockNewsClassification/src/main/java/de/uni_koeln/spinfo/stocknews/classification/workflow/WekaClassifier.java
@@ -178,12 +178,14 @@ public WekaClassifier(TreeMap<Integer, String> texts, File tdFile, ExperimentCon
 	private ZoneAbstractClassifier getClassifier(String classifierName) {
 		
 		switch(classifierName){
-		case "KNNClassifier" :
+		case "KNN" :
 			return new ZoneKNNClassifier();
-		case "RocchioClassifier" : 
+		case "R" : 
 			return new ZoneRocchioClassifier();
-		case "BayesClassifier" : 
+		case "NB" : 
 			return new ZoneNaiveBayesClassifier();
+		case "SVM" :
+			return new SVMClassifier();
 		default : 
 			return new ZoneNaiveBayesClassifier();	
 		}
